839fe23046c5534a9e47339c3f7b3ebd18c23b6d
Graham Christensen <graham@grahamc.com>
2013-09-24 10:41:49 -0500

    Fix the issue hwere the ipmitool recipe wasn't including the default recipe

--- recipes/ipmitool.rb
+++ recipes/ipmitool.rb
@@ -24,7 +24,7 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
-include_recipe "ohai-ipmi::ipmitool"
+include_recipe "ohai-ipmi::default"
 
 if node['ipmi']['available']
   package "ipmitool" do
